52465285ea599ad6b52f781477e3b362bc70ca9c
Travis T <travistokuyama@gmail.com>
2018-09-11 15:46:12 +0000

    Done.

---
+++ play.js
@@ -0,0 +1,14 @@
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
+
+var myScope = outerFunction();
+ 
+
+console.log(myScope())
